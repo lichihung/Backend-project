c15b0e852a7aabf6e89e59acbca1e7e4780adc52
lichihung <lichihun@umich.edu>
2025-11-02 21:42:57 -0800

    static

--- littlelemon/littlelemon/settings.py
+++ littlelemon/littlelemon/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'littlelemon.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['restaurant/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -114,8 +114,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = 'restaurant/static/'
 
+STATICFILES_DIRS = [
+    "restaurant/static",
+]
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
